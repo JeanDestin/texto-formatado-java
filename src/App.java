cca0a2fee3f4c6040f39e56382df2d1eaf7cd2f7
Jean David <destin.2206@aluno.pr.senac.br>
2024-04-04 21:44:04 -0300

    primeiro commit

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+//% substitui no texto um valor string
+System .out.printf("vai inprimir%s.% %\n","Um texto","qualquer
+//"%d substitui o coringa Por um numero inteiro no texto
+System .out.printf("Minha idade é :%d 33");
+//%.2fsubstitui o coringa %.2 por numero flutuante
+
